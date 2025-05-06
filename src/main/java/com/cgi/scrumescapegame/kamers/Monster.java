516096d9f63cb435722f3e4373fbd73dcb857237
Matt-hhs <24071765@student.hhs.nl>
2025-05-06 11:29:49 +0200

    Added Obstacles

---
+++ src/main/java/com/cgi/scrumescapegame/kamers/Monster.java
@@ -0,0 +1,15 @@
+package com.cgi.scrumescapegame.kamers;
+
+import com.cgi.scrumescapegame.Player;
+
+public class Monster implements Obstacle{
+    public boolean isOvercome;
+
+    public boolean getIsOvercome(){
+        return isOvercome;
+    }
+
+    public void attempt(Player p){
+
+    }
+}
