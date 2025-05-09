f2d6a75dfe8f855bb7c792dbf2ca2cefc193eed7
Matt-hhs <24071765@student.hhs.nl>
2025-05-09 12:55:37 +0200

    Monster added to Room 3

--- src/main/java/com/cgi/scrumescapegame/Room.java
+++ src/main/java/com/cgi/scrumescapegame/Room.java
@@ -41,25 +41,10 @@ public abstract class Room {
         speechBubble.append("╰").append("─".repeat(length + 2)).append("╯\n");
         speechBubble.append(" ".repeat(10)).append("  \\\n");
         speechBubble.append(" ".repeat(11)).append("  \\\n");
-        // ASCII Dier (Hond bijvoorbeeld)
-        String dog =
-                " / \\__\n" +
-                        "(    @\\___\n" +
-                        " /         O\n" +
-                        "/   (_____/\n" +
-                        "/_____/   U\n";
-        String cat =
-                " /\\_/\\\n" +
-                        "( o.o )\n" +
-                        " > ^ <\n";
-        String fish = "><(((('>\n";
-        // Voeg het dier toe, maar zorg ervoor dat het goed uitgelijnd is
-        int padding = 15; // Zorg ervoor dat het dier gecentreerd is
-        for (String line : fish.split("\n")) {
-            speechBubble.append(" ".repeat(padding)).append(line).append("\n");
-        }
+
         System.out.println(speechBubble);
 
+        ImagePrinter.printImage("matt.png");
     }
 }
 
