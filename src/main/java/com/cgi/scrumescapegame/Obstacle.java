2cc4435aca200e8c14c6e63c7dad394e915bd68b
Matt-hhs <24071765@student.hhs.nl>
2025-05-12 16:36:41 +0200

    add ReviewMonsters

--- src/main/java/com/cgi/scrumescapegame/Obstacle.java
+++ src/main/java/com/cgi/scrumescapegame/Obstacle.java
@@ -2,5 +2,8 @@ package com.cgi.scrumescapegame;
 
 public interface Obstacle {
     public boolean getIsOvercome();
+    void setIsOvercome(boolean isOvercome);
     public void attempt(Player p);
+    String getVraag();
+    String getImagepath();
 }
--- src/main/java/com/cgi/scrumescapegame/obstacles/ReviewMonster.java
+++ src/main/java/com/cgi/scrumescapegame/obstacles/ReviewMonster.java
@@ -1,13 +1,33 @@
 package com.cgi.scrumescapegame.obstacles;
 
+import com.cgi.scrumescapegame.MonsterAction;
 import com.cgi.scrumescapegame.Obstacle;
 import com.cgi.scrumescapegame.Player;
 
 public class ReviewMonster implements Obstacle {
     public boolean isOvercome;
+    private String vraag;
+    private String imagepath;
+
+    public ReviewMonster(String imagepath, String vraag){
+        this.imagepath = imagepath;
+        this.vraag = vraag;
+    }
 
     public boolean getIsOvercome(){
         return isOvercome;
+    }
+
+    public void setIsOvercome(boolean isOvercome){
+        this.isOvercome = isOvercome;
+    }
+
+    public String getImagepath(){
+        return imagepath;
+    }
+
+    public String getVraag(){
+        return vraag;
     }
 
     public void attempt(Player p){
