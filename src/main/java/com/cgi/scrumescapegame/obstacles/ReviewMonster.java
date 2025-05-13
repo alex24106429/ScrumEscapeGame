bb77aa1a228244fa2ac457172ecbc222300636f7
Robin <robinrid6@gmail.com>
2025-05-13 12:38:56 +0200

    matching puzzle

--- src/main/java/com/cgi/scrumescapegame/obstacles/ReviewMonster.java
+++ src/main/java/com/cgi/scrumescapegame/obstacles/ReviewMonster.java
@@ -1,8 +1,10 @@
 package com.cgi.scrumescapegame.obstacles;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import com.cgi.scrumescapegame.Obstacle;
+import com.cgi.scrumescapegame.vragen.Matching;
 import com.cgi.scrumescapegame.vragen.MeerkeuzeVraag;
 import com.cgi.scrumescapegame.Puzzle;
 import com.cgi.scrumescapegame.vragen.OpenVraag;
@@ -13,8 +15,10 @@ public class ReviewMonster implements Obstacle {
 
     public ReviewMonster() {
         puzzle = new Puzzle();
+        puzzle.addQuestion(new Matching("Match deze begrippen", Arrays.asList("Agile", "Scrum", "Sprint", "Product Owner"), Arrays.asList("Een iteratie van 1-4 weken", "Een framework voor Agile ontwikkeling", "Een rol binnen Scrum", "Een mindset"), Arrays.asList("1A", "2B", "3C", "4D")));
         puzzle.addQuestion(new OpenVraag("Dit is een test openvraag", "kaas"));
         puzzle.addQuestion(new MeerkeuzeVraag("Dit is een test vraag voor de review monster.", Arrays.asList("Optie A (correct)", "Optie B", "Optie C", "Optie D"), 0));
+
     }
 
     public Puzzle getPuzzle() {
