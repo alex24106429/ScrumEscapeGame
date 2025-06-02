f60c824ffb86a632cc487315765bfe47b1388549
alex24106429 <24106429@student.hhs.nl>
2025-06-02 16:15:47 +0200

    add printGradientTextWithShadow, printGradientTextWithOutline

--- src/main/java/com/cgi/scrumescapegame/Room.java
+++ src/main/java/com/cgi/scrumescapegame/Room.java
@@ -47,7 +47,7 @@ public abstract class Room {
     // Voor nu print het de beschrijving, kan later uitgebreid worden.
     public void enterRoom(Player player, Difficulty difficulty) {
         // System.out.println("\n--- " + getName() + " ---");
-		TextToImageRenderer.printGradientText(getName(), new Color(255, 255, 255), new Color(127, 127, 127), new Color(63, 63, 63), new Color(31, 31, 31), 2, true, false);
+		TextToImageRenderer.printGradientTextWithShadow(getName(), new Color(255, 255, 255), new Color(127, 127, 127), new Color(31, 31, 31), 2, true, false);
         PrintMethods.typeText(getDescription());
         player.printStatus();
         roomLogic(player, difficulty);
