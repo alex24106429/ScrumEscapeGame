203e29ffa3c81a2ec4bf1814a32f8176d97e8150
alex24106429 <24106429@student.hhs.nl>
2025-06-02 10:44:01 +0200

    make hintjoker print random funny hint

--- src/main/java/com/cgi/scrumescapegame/items/HintJoker.java
+++ src/main/java/com/cgi/scrumescapegame/items/HintJoker.java
@@ -2,11 +2,16 @@ package com.cgi.scrumescapegame.items;
 
 import com.cgi.scrumescapegame.Joker;
 import com.cgi.scrumescapegame.Room;
+import com.cgi.scrumescapegame.graphics.PrintMethods;
+import com.cgi.scrumescapegame.hints.FunnyHintProvider;
+import com.diogonunes.jcolor.Attribute;
 
 public class HintJoker extends Item implements Joker, LimitedUseItem {
+    FunnyHintProvider funnyHintProvider = new FunnyHintProvider();
+
     @Override
     public void useInRoom(Room room) {
-
+        PrintMethods.printlnColor(funnyHintProvider.getHint(), Attribute.BLUE_TEXT());
     }
 
     @Override
