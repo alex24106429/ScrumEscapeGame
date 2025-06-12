662386812c80d60b76d5a69e366465965fd17e27
alex24106429 <24106429@student.hhs.nl>
2025-06-12 12:45:35 +0200

    add backlog hydra

---
+++ src/main/java/com/cgi/scrumescapegame/enemies/BacklogHydra.java
@@ -0,0 +1,16 @@
+package com.cgi.scrumescapegame.enemies;
+
+import com.cgi.scrumescapegame.enemyattacks.BacklogHydra_DeadlineDoom;
+import com.cgi.scrumescapegame.enemyattacks.BacklogHydra_QueueQuake;
+
+public class BacklogHydra extends Enemy {
+    private static String name = "Backlog Hydra";
+    private static String imagePath = "monsters/backloghydra.png";
+    private static int maxHp = 70;
+    
+    public BacklogHydra() {
+        super(name, imagePath, maxHp);
+        behaviors.add(new BacklogHydra_DeadlineDoom());
+        behaviors.add(new BacklogHydra_QueueQuake());
+    }
+}
---
+++ src/main/java/com/cgi/scrumescapegame/enemyattacks/BacklogHydra_DeadlineDoom.java
@@ -0,0 +1,23 @@
+package com.cgi.scrumescapegame.enemyattacks;
+
+import com.cgi.scrumescapegame.Player;
+import com.cgi.scrumescapegame.enemies.AttackBehavior;
+import com.cgi.scrumescapegame.enemies.Enemy;
+
+import java.util.Random;
+
+public class BacklogHydra_DeadlineDoom implements AttackBehavior {
+	private Random rand = new Random();
+
+	public String getName() {
+		return "Deadline Doom";
+	}
+
+	public int attack(Enemy enemy, Player player) {
+		int base = 30;
+		int var = base / 5;
+		int dmg = base - var + rand.nextInt(var * 2 + 1);
+		player.changeHp( - dmg);
+		return dmg;
+	}
+}
